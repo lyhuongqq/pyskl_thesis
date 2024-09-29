a279b8dc8d286034bf38ef901e0695072b35bd22
lyhuongqq <163553326+lyhuongqq@users.noreply.github.com>
2024-09-29 15:28:19 +0700

    Update base.py

--- pyskl/datasets/base.py
+++ pyskl/datasets/base.py
@@ -11,6 +11,8 @@ from abc import ABCMeta, abstractmethod
 from collections import OrderedDict, defaultdict
 from mmcv.utils import print_log
 from torch.utils.data import Dataset
+from sklearn.metrics import precision_score, recall_score, f1_score
+from sklearn.metrics import precision_recall_fscore_support
 
 import sys
 # Add the parent directory to the sys.path
@@ -20,7 +22,7 @@ from pyskl.smp import auto_mix2
 from pyskl.core import mean_average_precision, mean_class_accuracy, top_k_accuracy
 #from .pipelines import Compose
 from pyskl.datasets.pipelines import Compose
-from sklearn.metrics import precision_score, recall_score, f1_score
+
 
 class BaseDataset(Dataset, metaclass=ABCMeta):
     """Base class for datasets.
@@ -118,22 +120,17 @@ class BaseDataset(Dataset, metaclass=ABCMeta):
                  metric_options=dict(top_k_accuracy=dict(topk=(1, 5))),
                  logger=None,
                  **deprecated_kwargs):
-        """Perform evaluation for common datasets.
+        """Perform evaluation for common datasets, including precision, recall, F1-score.
 
         Args:
             results (list): Output results.
-            metrics (str | sequence[str]): Metrics to be performed.
-                Defaults: 'top_k_accuracy'.
-            metric_options (dict): Dict for metric options. Options are
-                ``topk`` for ``top_k_accuracy``.
-                Default: ``dict(top_k_accuracy=dict(topk=(1, 5)))``.
-            logger (logging.Logger | None): Logger for recording.
-                Default: None.
+            metrics (str | sequence[str]): Metrics to be performed. Defaults: 'top_k_accuracy'.
+            metric_options (dict): Dict for metric options. Default: ``dict(top_k_accuracy=dict(topk=(1, 5)))``.
+            logger (logging.Logger | None): Logger for recording. Default: None.
             deprecated_kwargs (dict): Used for containing deprecated arguments.
-                See 'https://github.com/open-mmlab/mmaction2/pull/286'.
 
         Returns:
-            dict: Evaluation results dict.
+            dict: Evaluation results dict, including precision, recall, and f1-score.
         """
         if not isinstance(results, list):
             raise TypeError(f'results must be a list, but got {type(results)}')
@@ -141,44 +138,8 @@ class BaseDataset(Dataset, metaclass=ABCMeta):
             f'The length of results is not equal to the dataset len: '
             f'{len(results)} != {len(self)}')
 
-        if isinstance(results[0], list) or isinstance(results[0], tuple):
-            num_results = len(results[0])
-            eval_results = dict()
-            for i in range(num_results):
-                eval_results_cur = self.evaluate(
-                    [x[i] for x in results], metrics, metric_options, logger, **deprecated_kwargs)
-                eval_results.update({f'{k}_{i}': v for k, v in eval_results_cur.items()})
-            return eval_results
-
-        elif isinstance(results[0], dict):
-            eval_results = dict()
-            for key in results[0]:
-                results_cur = [x[key] for x in results]
-                eval_results_cur = self.evaluate(results_cur, metrics, metric_options, logger, **deprecated_kwargs)
-                eval_results.update({f'{key}_{k}': v for k, v in eval_results_cur.items()})
-            # Ad-hoc for RGBPoseConv3D
-            if len(results[0]) == 2 and 'rgb' in results[0] and 'pose' in results[0]:
-                rgb = [x['rgb'] for x in results]
-                pose = [x['pose'] for x in results]
-                preds = auto_mix2([rgb, pose])
-                for k in preds:
-                    eval_results_cur = self.evaluate(preds[k], metrics, metric_options, logger, **deprecated_kwargs)
-                    eval_results.update({f'RGBPose_{k}_{key}': v for key, v in eval_results_cur.items()})
-
-            return eval_results
-
-        # Protect ``metric_options`` since it uses mutable value as default
-        metric_options = copy.deepcopy(metric_options)
-        if deprecated_kwargs != {}:
-            warnings.warn(
-                'Option arguments for metrics has been changed to '
-                "`metric_options`, See 'https://github.com/open-mmlab/mmaction2/pull/286' "  # noqa: E501
-                'for more details')
-            metric_options['top_k_accuracy'] = dict(
-                metric_options['top_k_accuracy'], **deprecated_kwargs)
-
         metrics = metrics if isinstance(metrics, (list, tuple)) else [metrics]
-        allowed_metrics = ['top_k_accuracy', 'mean_class_accuracy', 'mean_average_precision']
+        allowed_metrics = ['top_k_accuracy', 'mean_class_accuracy', 'mean_average_precision', 'precision', 'recall', 'f1_score']
 
         for metric in metrics:
             if metric not in allowed_metrics:
@@ -187,6 +148,10 @@ class BaseDataset(Dataset, metaclass=ABCMeta):
         eval_results = OrderedDict()
         gt_labels = [ann['label'] for ann in self.video_infos]
 
+        # Ensure metric_options is deepcopied to avoid mutable default
+        metric_options = copy.deepcopy(metric_options)
+
+        # Evaluate each metric
         for metric in metrics:
             msg = f'Evaluating {metric} ...'
             if logger is None:
@@ -194,41 +159,41 @@ class BaseDataset(Dataset, metaclass=ABCMeta):
             print_log(msg, logger=logger)
 
             if metric == 'top_k_accuracy':
-                topk = metric_options.setdefault('top_k_accuracy',
-                                                 {}).setdefault(
-                                                     'topk', (1, 5))
-                if not isinstance(topk, (int, tuple)):
-                    raise TypeError('topk must be int or tuple of int, '
-                                    f'but got {type(topk)}')
+                topk = metric_options.setdefault('top_k_accuracy', {}).setdefault('topk', (1, 5))
                 if isinstance(topk, int):
                     topk = (topk, )
-
                 top_k_acc = top_k_accuracy(results, gt_labels, topk)
-                log_msg = []
                 for k, acc in zip(topk, top_k_acc):
                     eval_results[f'top{k}_acc'] = acc
-                    log_msg.append(f'\ntop{k}_acc\t{acc:.4f}')
-                log_msg = ''.join(log_msg)
-                print_log(log_msg, logger=logger)
-                continue
+                    print_log(f'\ntop{k}_acc\t{acc:.4f}', logger=logger)
 
             if metric == 'mean_class_accuracy':
                 mean_acc = mean_class_accuracy(results, gt_labels)
                 eval_results['mean_class_accuracy'] = mean_acc
-                log_msg = f'\nmean_acc\t{mean_acc:.4f}'
-                print_log(log_msg, logger=logger)
-                continue
+                print_log(f'\nmean_acc\t{mean_acc:.4f}', logger=logger)
 
             if metric == 'mean_average_precision':
-                gt_labels_arrays = [
-                    self.label2array(self.num_classes, label)
-                    for label in gt_labels
-                ]
+                gt_labels_arrays = [self.label2array(self.num_classes, label) for label in gt_labels]
                 mAP = mean_average_precision(results, gt_labels_arrays)
                 eval_results['mean_average_precision'] = mAP
-                log_msg = f'\nmean_average_precision\t{mAP:.4f}'
-                print_log(log_msg, logger=logger)
-                continue
+                print_log(f'\nmean_average_precision\t{mAP:.4f}', logger=logger)
+
+            if metric in ['precision', 'recall', 'f1_score']:
+                pred_labels = np.argmax(results, axis=1)
+                precision, recall, f1, _ = precision_recall_fscore_support(gt_labels, pred_labels, average=None, labels=np.arange(self.num_classes))
+
+                for i in range(self.num_classes):
+                    eval_results[f'{metric}_class_{i}'] = eval(metric[i])
+                    print_log(f'\n{metric}_class_{i}\t{eval_results[f"{metric}_class_{i}"]:.4f}', logger=logger)
+
+                # Compute overall metrics across all classes
+                precision_macro, recall_macro, f1_macro, _ = precision_recall_fscore_support(gt_labels, pred_labels, average='macro')
+                eval_results['precision_macro'] = precision_macro
+                eval_results['recall_macro'] = recall_macro
+                eval_results['f1_macro'] = f1_macro
+                print_log(f'\nprecision_macro\t{precision_macro:.4f}', logger=logger)
+                print_log(f'\nrecall_macro\t{recall_macro:.4f}', logger=logger)
+                print_log(f'\nf1_macro\t{f1_macro:.4f}', logger=logger)
 
         return eval_results
 
